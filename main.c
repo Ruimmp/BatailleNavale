21337f28799f69f0ec3d261ec67620cfe2899d77
Ruimmp <61775818+Ruimmp@users.noreply.github.com>
2021-02-18 12:22:34 +0100

    Mise a jour de la documentation et débout d'un petit code sur CLion
    Dans ce commit, j'ai un peu commencé le code, mais avec beaucoup d'échecs.
    J'ai voulu faire un menu pour le débout.
    
    La documentation, j'ai mis a jour le journal de travail, commencé le MCD pourtant je crois qu'il me manque encore des tables.

--- main.c
+++ main.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <windows.h>
+
+void hub() {
+    //Initialisation des variables
+    int choix;
+    //Initialisation des variables
+
+    do {
+        printf("Bataille Navale!\n\n\n\n");
+        printf("Options:\n");
+        printf("1-jouer\n2-classement\n3-aide\n4-quitter");
+        scanf("%d", &choix);
+    } while (choix < 1 || choix > 3);
+    switch (choix) {
+        case 1:
+            if (choix == 1)
+                //jeu();
+                printf("Jeu");
+            break;
+        case 2:
+            if (choix == 2)
+                //classement();
+                printf("classement");
+            break;
+        case 3:
+            if (choix == 3)
+                //aide();
+                printf("aide");
+            break;
+        case 4:
+            if (choix == 4)
+                system("pause");
+            break;
+        case 5:
+            if (choix < 1){
+                printf("Veillez entrer une valeur parmis celles notées dans les options.");
+            }
+            break;
+    }
+}
+
 
 int main() {
-    printf("bbbbbbbbbbbbbbbbbbbb");
+    hub();
     return 0;
 }
